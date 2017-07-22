9e6fd3726995940c444fd40a8ac3bf3dd53d1cc9
SayHelloAllen <592470261@qq.com>
2017-07-22 18:56:46 +0800

    删除 Customer.java Change-Id: Ib7c33e19d11d941c0b2f29883946a80504a80871 Signed-off-by: SayHelloAllen <592470261@qq.com>

--- Customer.java
+++
@@ -1,9 +0,0 @@
-package com.banner.bean;
-
-public class Customer {
-	public static void main(String[] args) {
-		System.out.println("Hello222222!");
-		int a = 123535;
-		System.out.println(a);
-	}
-}
